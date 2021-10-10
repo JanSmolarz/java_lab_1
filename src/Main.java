923dfbb04280638ba9d2e3e6e48ab9d01c6e0463
Jan <jan.smolarz@yahoo.com>
2021-10-10 22:39:43 +0200

    przyklad 1 chyba zrobiony ćw.2 podpunkt a) zrobione b) zrobione ćw.3 zad.1 a) zrobione b) zrobione zad.2 zrobione cw4

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
-
+        boolean a = false;
+        boolean b = true;
+        boolean c = false;
     }
 }
