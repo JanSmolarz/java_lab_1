90490bbd8f2baa78350bcb87d1ac5ddacd02cb62
Jan <jan.smolarz@yahoo.com>
2021-10-12 19:30:46 +0200

    przyklad 1 chyba zrobiony ćw.2 podpunkt a) zrobione b) zrobione ćw.3 zad.1 a) zrobione b) zrobione zad.2 zrobione cw4 zrobione

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,23 @@ public class Main {
         /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Podaj swój wiek: ");
+        int wiek = scan.nextInt();
+
+        int modulo = wiek%3;
+        if (modulo == 0) {
+            System.out.println("Twój wiek jest podzielny przez 3");
+        }else {
+            System.out.println("Twój wiek nie dzieli się przez 3");
+        }
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+        System.out.println("Podaj swój numer albumu: ");
+        int indeks = scan.nextInt();
+        int dzielnik = 2;
+
 
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
