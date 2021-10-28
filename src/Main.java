2d20a44a877e6345f03ddb9ab28801057847c9ec
Jan <jan.smolarz@yahoo.com>
2021-10-28 17:24:50 +0200

    przyklad 1 chyba zrobiony ćw.1 ćw.2 podpunkt a) zrobione b) zrobione ćw.3 zad.1 a) zrobione b) zrobione zad.2 zrobione cw4 zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
-
+        System.out.println("przykład1");
+        System.out.printf("Przykład2 \n");
+        System.out.print("Przykład3");
     }
 }
