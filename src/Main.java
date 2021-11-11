c6f58d40e0d330b4e7b7d0e4975d7f49290ec343
Jan <jan.smolarz@yahoo.com>
2021-11-11 18:27:46 +0100

    cw 6 zrobione

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,20 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        double[] tab1 = new double[5];
+        tab1[0] = 1.21;
+        tab1[1] = 2.98;
+        tab1[2] = 3.1451;
+        tab1[3] = 4.0123;
+        tab1[4] = 5.1207;
+
+        System.out.println("Tablica ma " + tab1.length + " elementów"); // liczenie elementów tablicy
+
+        System.out.println("Tablica 1:");
+        for (int i = 0; i< tab1.length; i++){
+            System.out.println(tab1[i] + " ");
+        }
+
 
 
     }
